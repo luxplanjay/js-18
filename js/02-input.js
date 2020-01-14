bb778dbadd51d2f2366abda141a25a7a7b7f904b
Alexander Repeta <alexander.repeta@gmail.com>
2020-01-14 21:15:28 +0200

    Add examples

---
+++ js/02-input.js
@@ -0,0 +1,26 @@
+/*
+ * Ввод пользовователя
+ */
+
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// console.log(shouldRenew);
+
+// let userInput = prompt('Введите число 10');
+// userInput = Number(userInput);
+// console.log(typeof userInput);
+// console.log(userInput === 10);
+
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// const userInput = prompt('Введите ваше имя');
+
+// console.log(userInput);
+
+// const userInput = prompt('Введите число 5!');
+
+// console.log(userInput);
+// console.log(typeof userInput);
+
+// const number = Number(userInput);
+// console.log(number);
+// console.log(typeof number);
+// console.log(number === 5);
---
+++ js/04-strings.js
@@ -0,0 +1,36 @@
+/*
+ * Шаблонные строки
+ */
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
+// const room = 716;
+// const type = 'VIP';
+// Гость x y поселяется в z номер q
+
+// const message =
+//   'Гость ' +
+//   firstName +
+//   ' ' +
+//   lastName +
+//   ' поселяется в ' +
+//   type +
+//   ' номер ' +
+//   room;
+
+// const message = `Гость ${firstName} ${lastName} поселяется в ${type} номер ${room}`;
+
+// console.log(message.length);
+
+/*
+ * Методы строк
+ */
+// const superValue = 'qwerty';
+
+// const userInput = prompt('Введи то что надо');
+// console.log('userinput before: ', userInput);
+
+// const normalizedInput = userInput.toLowerCase();
+// console.log('userinput after: ', userInput);
+// console.log('normalizedInput: ', normalizedInput);
+
+// console.log(superValue === normalizedInput);
